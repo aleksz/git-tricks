8e6cab17592632a1b3a551a7c879399a7d18c819
Aleksandr Zhuikov <aleksandr.zhuikov@gmail.com>
2013-01-02 20:04:35 +0200

    removed merge artifacts

--- src/ee/ignite/MySuperClass.java
+++ src/ee/ignite/MySuperClass.java
@@ -6,11 +6,7 @@ public class MySuperClass {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("merged");
-=======
-		System.out.println("change in git #5");
->>>>>>> branch 'master' of ssh://git@github.com/aleksz/git-tricks.git
 	}
 
 }
