7113ed11deb1bad890d1a24ee7e21716a803e733
Aleksandr Zhuikov <aleksandr.zhuikov@gmail.com>
2013-01-02 19:55:06 +0200

    reverted to prev revision

--- src/ee/ignite/MySuperClass.java
+++ src/ee/ignite/MySuperClass.java
@@ -6,11 +6,7 @@ public class MySuperClass {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("merged");
-=======
 		System.out.println("oops, I did it again");
->>>>>>> branch 'master' of ssh://git@github.com/aleksz/git-tricks.git
 	}
 
 }
