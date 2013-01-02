fd9286afad21941587441d12206c9d1eff550928
Aleksandr Zhuikov <aleksandr.zhuikov@gmail.com>
2013-01-02 19:59:57 +0200

    conflicting commit

--- src/ee/ignite/MySuperClass.java
+++ src/ee/ignite/MySuperClass.java
@@ -6,7 +6,7 @@ public class MySuperClass {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("oops, I did it again");
+		System.out.println("conflict");
 	}
 
 }
