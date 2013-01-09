f718adaf60b5edf6b8763103f67bdd705c817991
Aleksandr Zhuikov <aleksandr.zhuikov@gmail.com>
2013-01-09 20:04:09 +0200

    made local change

--- src/ee/ignite/MySuperClass.java
+++ src/ee/ignite/MySuperClass.java
@@ -6,7 +6,7 @@ public class MySuperClass {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("merged conflict");
+		System.out.println("local change");
 	}
 
 }
