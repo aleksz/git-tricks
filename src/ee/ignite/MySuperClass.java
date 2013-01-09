460c87a244761262dda6948e00d58258f5792a87
Aleksandr Zhuikov <aleksandr.zhuikov@gmail.com>
2013-01-09 21:51:03 +0200

    Update src/ee/ignite/MySuperClass.java

--- src/ee/ignite/MySuperClass.java
+++ src/ee/ignite/MySuperClass.java
@@ -7,6 +7,6 @@ public class MySuperClass {
 	 */
 	public static void main(String[] args) {
 		System.out.println("merged change");
-		System.out.println("merged change");
+		System.out.println("non conflicting");
 	}
 }
