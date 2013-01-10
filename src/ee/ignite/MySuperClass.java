dbdace0644e4b1100c8e842df0df1a0957a01735
Aleksandr Zhuikov <aleksandr.zhuikov@gmail.com>
2013-01-10 21:09:40 +0200

    first

--- src/ee/ignite/MySuperClass.java
+++ src/ee/ignite/MySuperClass.java
@@ -7,8 +7,6 @@ public class MySuperClass {
 	 */
 	public static void main(String[] args) {
 		System.out.println("non conflicting");
-		System.out.println("conflict");
-		System.out.println("234");
 	}
 	
 	private void nonConflicting() {
