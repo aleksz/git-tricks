db3589b0d1a58f71f9feb708194af202b13f29fa
Aleksandr Zhuikov <aleksandr.zhuikov@gmail.com>
2013-01-10 21:16:28 +0200

    remote conflict that should conflict

--- src/ee/ignite/MySuperClass.java
+++ src/ee/ignite/MySuperClass.java
@@ -7,7 +7,7 @@ public class MySuperClass {
 	 */
 	public static void main(String[] args) {
 		System.out.println("non conflicting");
-		System.out.println();
+		System.out.println("conflict");
 		System.out.println("234");
 	}
 	
