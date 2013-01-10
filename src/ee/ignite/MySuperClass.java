a1de1da74ec53bd23bbdab552ffd6e0529ac1277
Aleksandr Zhuikov <aleksandr.zhuikov@gmail.com>
2013-01-10 21:09:59 +0200

    second

--- src/ee/ignite/MySuperClass.java
+++ src/ee/ignite/MySuperClass.java
@@ -7,6 +7,7 @@ public class MySuperClass {
 	 */
 	public static void main(String[] args) {
 		System.out.println("non conflicting");
+		System.out.println("!!!!!!!!!!");
 	}
 	
 	private void nonConflicting() {
