c2f65c730d6b12b2dbd391555d96d4da1e83741c
Aleksandr Zhuikov <aleksandr.zhuikov@gmail.com>
2013-01-10 20:40:50 +0200

    latest change

--- src/ee/ignite/MySuperClass.java
+++ src/ee/ignite/MySuperClass.java
@@ -6,7 +6,6 @@ public class MySuperClass {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("merged change");
 		System.out.println("non conflicting");
 		System.out.println();
 		System.out.println("234");
