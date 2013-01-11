aaf8d07881174a180ed9a66beb319559e315cd32
Aleksandr Zhuikov <aleksandr.zhuikov@gmail.com>
2013-01-11 22:45:12 +0200

    cleanup

--- src/ee/ignite/MySuperClass.java
+++ src/ee/ignite/MySuperClass.java
@@ -1,23 +1,10 @@
 package ee.ignite;
 
-//
-//
 public class MySuperClass {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("non conflicting");
-		System.out.println("!!!!!!!!!!");
-		System.out.println("??????????");
-	}
-
-	private void nonConflicting() {
-		System.out.println("merged conflict");
-		System.out.println("change based on remote commit");
-		System.out.println("local change");
 	}
 }
-//
-//
